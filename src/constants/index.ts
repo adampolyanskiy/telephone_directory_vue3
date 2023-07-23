29ba69f5c51f13fbcf30a3541672f6e706aa301b
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 05:24:09 +0600

    add modal names and messages

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,5 @@ export * from '@/constants/error-descriptor';
 export * from '@/constants/errors';
 export * from '@/constants/dependency-keys';
 export * from '@/constants/validation';
+export * from '@/constants/messages';
+export * from '@/constants/modals';
---
+++ src/constants/modals.ts
@@ -0,0 +1,5 @@
+export const MODAL_NAMES = {
+  CONTACT_UPDATE: '',
+  CONTACT_ADD: '',
+  CONTACT_DELETE: ''
+};
