3c41e210acdb88af3c1b02b5be677c8ec5792003
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 04:39:40 +0600

    add filter and generate random contact methods

--- src/interfaces/contact-service.ts
+++ src/interfaces/contact-service.ts
@@ -1,3 +1,6 @@
 import type { Contact, DataService } from './index';
 
-export interface ContactService extends DataService<Contact, number> {}
+export interface ContactService extends DataService<Contact, number> {
+  filterContacts(contacts: Contact[], filter: string): Contact[];
+  generateRandomContact(): Contact;
+}
--- src/services/contact-service.ts
+++ src/services/contact-service.ts
@@ -1,6 +1,7 @@
 import { ERRORS } from '@/constants/errors';
 import type { Contact, ContactService as IContactService } from '@/interfaces';
 import { LocalDB, db } from '@/models/db';
+import { fakerRU as faker } from '@faker-js/faker';
 
 export class ContactService implements IContactService {
   constructor(private db: LocalDB = db) {}
@@ -31,6 +32,35 @@ export class ContactService implements IContactService {
     }
 
     return await this.db.contacts.delete(id);
+  }
+
+  filterContacts(contacts: Contact[], filter: string): Contact[] {
+    return contacts.filter((contact) => {
+      for (const key in contact) {
+        const value = contact[key];
+
+        if (typeof value !== 'string') {
+          continue;
+        }
+
+        if ((value as string).toLocaleUpperCase().indexOf(filter.toLocaleUpperCase()) > -1) {
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+
+  generateRandomContact(): Contact {
+    return {
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      middlename: faker.person.middleName(),
+      cellphone: faker.phone.number(),
+      homephone: faker.phone.number(),
+      address: faker.location.city()
+    };
   }
 }
 
