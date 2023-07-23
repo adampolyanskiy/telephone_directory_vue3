c52a1718d4ef3e72834d654ef60b77881014586a
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 17:01:17 +0600

    provide contact service singleton

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import './assets/styles/main.css';
+import { ContactService as ContactServiceSymbol } from './constants';
+import { contactService } from './services/contact-service';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.provide(ContactServiceSymbol, contactService);
+
+app.mount('#app');
