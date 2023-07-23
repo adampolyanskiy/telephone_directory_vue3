1f6d89de9d547c9e6db894bb9bb0c72794085fa3
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 14:21:21 +0600

    remove code from descriptor

--- src/constants/error-descriptor.ts
+++ src/constants/error-descriptor.ts
@@ -1,13 +1,11 @@
 class ErrorDescriptor {
-  code: number;
   message: string;
 
-  constructor(code: number, message: string) {
-    this.code = code;
+  constructor(message: string) {
     this.message = message;
   }
 }
 
 export const ERRORS = {
-  NO_ITEM_ID: new ErrorDescriptor(1, 'Item id is not specified')
+  NO_ITEM_ID: new ErrorDescriptor('Item id is not specified')
 };
