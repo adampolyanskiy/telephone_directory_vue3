9b0ab32149abb36786ef342a6c27205dd0b34d6f
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 21:19:42 +0600

    implement and globalize filter input component

--- src/components/ui/FilterInput.vue
+++ src/components/ui/FilterInput.vue
@@ -1,6 +1,38 @@
 <script setup lang="ts">
+const $props = defineProps<{
+  modelValue?: string;
+}>();
+
+const $emit = defineEmits<{
+  'update:modelValue': [value: string];
+}>();
 </script>
 
-<template></template>
+<template>
+  <form>
+    <div class="relative">
+      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg
+          class="w-4 h-4 text-gray-500 dark:text-gray-400"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+        </svg>
+      </div>
+      <AppTextInput
+        class="p-4 pl-10"
+        placeholder="Фильтр"
+        :model-value="$props.modelValue"
+        @update:model-value="$emit('update:modelValue', $event)"></AppTextInput>
+    </div>
+  </form>
+</template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './assets/styles/main.css';
 import AppButton from './components/ui/AppButton.vue';
 import AppTextInput from './components/ui/AppTextInput.vue';
+import FilterInput from './components/ui/FilterInput.vue';
 import { ContactService as ContactServiceSymbol } from './constants';
 import { contactService } from './services/contact-service';
 
@@ -13,5 +14,6 @@ app.provide(ContactServiceSymbol, contactService);
 
 app.component('AppButton', AppButton);
 app.component('AppTextInput', AppTextInput);
+app.component('FilterInput', FilterInput);
 
 app.mount('#app');
