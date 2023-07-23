4504e46171f325641324b9eb71c2919aebb4a5f8
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 02:52:51 +0600

    add validation errors constant

---
+++ src/constants/validation.ts
@@ -0,0 +1,7 @@
+export const VALIDATION_MESSAGES = {
+  REQUIRED_FIELD: 'Поле обязательно',
+  MIN_LENGTH: 'Поле должно содержать минимум {0} символов',
+  MAX_LENGTH: 'Поле должно содержать максимум {0} символов',
+  CYRILLIC_ONLY: 'Поле должно содержать только кириллицу',
+  PHONE_LENGTH: 'Поле обязательно'
+};
