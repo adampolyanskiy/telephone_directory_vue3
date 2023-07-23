1865d7e893567d41fdba93c527c600130ede0b4f
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 19:31:15 +0600

    rename input component and implement basic input wrapper

---
+++ src/components/ui/AppTextInput.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const $props = defineProps<{
+  label?: string;
+  modelValue?: string;
+}>();
+
+console.log($props);
+
+const $emit = defineEmits<{
+  'update:modelValue': [value: string];
+}>();
+
+function updateModelValue(e: Event) {
+  $emit('update:modelValue', (e.target as HTMLInputElement).value);
+}
+</script>
+
+<template>
+  <label
+    v-if="$props.label"
+    :for="($attrs.id as string | undefined)"
+    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+    {{ $props.label }}
+  </label>
+  <input
+    :id="($attrs.id as string | undefined)"
+    type="text"
+    :value="$props.modelValue"
+    :placeholder="($attrs.placeholder as string | undefined)"
+    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    @input="updateModelValue" />
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/styles/main.css';
 import AppButton from './components/ui/AppButton.vue';
+import AppTextInput from './components/ui/AppTextInput.vue';
 import { ContactService as ContactServiceSymbol } from './constants';
 import { contactService } from './services/contact-service';
 
@@ -11,5 +12,6 @@ const app = createApp(App);
 app.provide(ContactServiceSymbol, contactService);
 
 app.component('AppButton', AppButton);
+app.component('AppTextInput', AppTextInput);
 
 app.mount('#app');
