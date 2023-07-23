35e0dee6ffafcac3a3a754e9810793b35c25a947
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 17:41:51 +0600

    register app button component

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import './assets/styles/main.css';
+import AppButton from './components/ui/AppButton.vue';
 import { ContactService as ContactServiceSymbol } from './constants';
 import { contactService } from './services/contact-service';
 
 const app = createApp(App);
 
 app.provide(ContactServiceSymbol, contactService);
+
+app.component('AppButton', AppButton);
 
 app.mount('#app');
