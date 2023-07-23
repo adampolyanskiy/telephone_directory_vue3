8d695caebd16b630a2148b5805b6f3fec69c58af
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 12:52:40 +0600

    add contact interface and index.ts file for indexing

---
+++ src/interfaces/contact.ts
@@ -0,0 +1,9 @@
+export interface Contact {
+    id: number;
+    firstname: string;
+    lastname: string;
+    middlename: string;
+    address: string;
+    homephone: string;
+    cellphone: string;
+}
