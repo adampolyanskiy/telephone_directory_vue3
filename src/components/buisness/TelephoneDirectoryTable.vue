bd7e32ae783ed21abad77f7776e4cfd2c67942ee
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 03:32:00 +0600

    implement telephone directory component

--- src/components/buisness/TelephoneDirectoryTable.vue
+++ src/components/buisness/TelephoneDirectoryTable.vue
@@ -1,6 +1,48 @@
 <script setup lang="ts">
+import type { Contact } from '@/interfaces';
+
+import AppButton from '../ui/AppButton.vue';
+
+defineProps<{
+  contacts: Contact[];
+}>();
+
+defineEmits<{
+  contactUpdate: [contact: Contact];
+  contactDelete: [contact: number];
+}>();
 </script>
 
-<template></template>
+<template>
+  <AppTable
+    :items="contacts"
+    :field-names="[
+      'firstname',
+      'middlename',
+      'lastname',
+      'address',
+      'homephone',
+      'cellphone',
+      'actions'
+    ]"
+    :column-names="[
+      'Имя',
+      'Фамилия',
+      'Отчество',
+      'Адрес',
+      'Домашний телефон',
+      'Мобильный телефон',
+      ''
+    ]">
+    <template #actions="{ item }">
+      <AppButton
+        class="mr-4"
+        @click="$emit('contactUpdate', item)">
+        Изменить
+      </AppButton>
+      <AppButton @click="$emit('contactUpdate', item.id)"> Удалить </AppButton>
+    </template>
+  </AppTable>
+</template>
 
 <style scoped></style>
