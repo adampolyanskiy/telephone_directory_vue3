e9ab046067466bbeb36a1ad727ca04c2aad0ea53
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 04:43:38 +0600

    add filter & generate random contact functonality

--- src/components/buisness/TelephoneDirectory.vue
+++ src/components/buisness/TelephoneDirectory.vue
@@ -1,6 +1,49 @@
 <script setup lang="ts">
+import TelephoneDirectoryTable from '@/components/buisness/TelephoneDirectoryTable.vue';
+import AppButton from '@/components/ui/AppButton.vue';
+import FilterInput from '@/components/ui/FilterInput.vue';
+import { ContactService } from '@/constants';
+import type { Contact, ContactService as IContactService } from '@/interfaces';
+import { inject, reactive, ref, watch } from 'vue';
+
+const contactService = inject<IContactService>(ContactService);
+
+const filter = ref<string>();
+
+const contacts = reactive<Contact[]>([]);
+
+let filteredContacts = contacts;
+
+function generateRandomContact() {
+  contacts.push(contactService!.generateRandomContact());
+}
+
+watch(filter, () => {
+  filteredContacts = contactService!.filterContacts(contacts, filter.value!);
+});
 </script>
 
-<template></template>
+<template>
+  <main class="p-10">
+    <div class="bg-gray-50 mb-4 p-3 rounded-lg drop-shadow-md flex">
+      <AppButton class="mr-3">Добавить</AppButton>
+      <AppButton
+        class="mr-3"
+        @click="generateRandomContact()"
+        >Сгенерировать</AppButton
+      >
+      <FilterInput
+        v-model.trim="filter"
+        class="basis-1/3">
+      </FilterInput>
+    </div>
+    <div>
+      <TelephoneDirectoryTable
+        class="rounded-lg drop-shadow-lg"
+        :contacts="filteredContacts">
+      </TelephoneDirectoryTable>
+    </div>
+  </main>
+</template>
 
 <style scoped></style>
