7cf1392f8cc27d3a49a90e0c655f6e55cd3c487c
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 03:10:04 +0600

    add other constant for validation

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -1,7 +1,43 @@
 export const VALIDATION_MESSAGES = {
   REQUIRED_FIELD: 'Поле обязательно',
-  MIN_LENGTH: 'Поле должно содержать минимум {0} символов',
-  MAX_LENGTH: 'Поле должно содержать максимум {0} символов',
+  MIN_LENGTH: 'Поле должно содержать минимум {0} символ(ов)',
+  MAX_LENGTH: 'Поле должно содержать максимум {0} символ(ов)',
   CYRILLIC_ONLY: 'Поле должно содержать только кириллицу',
   PHONE_LENGTH: 'Поле обязательно'
+};
+
+export const VALIDATION_LEN = {
+  FIRSTNAME: {
+    MIN: 2,
+    MAX: 20
+  },
+  LASTNAME: {
+    MIN: 2,
+    MAX: 20
+  },
+  MIDDLENAME: {
+    MIN: 2,
+    MAX: 20
+  },
+  FIRST_NAME: {
+    MIN: 2,
+    MAX: 20
+  },
+  ADDRESS: {
+    MIN: 2,
+    MAX: 11
+  },
+  CELLPHONE: {
+    MIN: 11,
+    MAX: 11
+  },
+  HOMEPHONE: {
+    MIN: 11,
+    MAX: 11
+  }
+};
+
+export const VALIDATION_REGEX = {
+  CYRILLIC: /^[А-Яа-яёЁ]+$/,
+  TELEPHONE: /[0-9]{11,11}/
 };
