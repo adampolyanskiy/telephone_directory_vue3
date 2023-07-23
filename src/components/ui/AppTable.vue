2ab148544b8731c3895c8137929a6eeffbc21b5d
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-23 23:12:20 +0600

    add basic table component that support custom cell templates

--- src/components/ui/AppTable.vue
+++ src/components/ui/AppTable.vue
@@ -1,6 +1,44 @@
 <script setup lang="ts">
+defineProps<{
+  fieldNames?: string[];
+  items: any[];
+}>();
 </script>
 
-<template></template>
+<template>
+  <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th
+            v-for="fieldName in fieldNames"
+            :key="fieldName"
+            scope="col"
+            class="px-6 py-3">
+            {{ fieldName }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in items"
+          :key="item.id"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <td
+            v-for="fieldName in fieldNames"
+            :key="fieldName"
+            scope="row"
+            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <slot
+              :name="fieldName"
+              :item="item">
+              {{ item[fieldName] }}
+            </slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 
 <style scoped></style>
