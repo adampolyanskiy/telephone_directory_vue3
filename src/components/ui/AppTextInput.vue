e679971c31612699cb56106c46a9c73358d41f0b
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 13:00:26 +0600

    fix

--- src/components/ui/AppTextInput.vue
+++ src/components/ui/AppTextInput.vue
@@ -23,7 +23,7 @@ function updateModelValue(e: Event) {
 <template>
   <label
     v-if="$props.label"
-    :for="($attrs.id as string | undefined)"
+    :for="$attrs.id"
     :class="[
       'block mb-2 text-sm font-medium',
       valid && 'text-gray-900 dark:text-white',
