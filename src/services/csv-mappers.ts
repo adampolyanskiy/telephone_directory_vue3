222cf3f38c4d9f59e8aabd6704e369046c0cc825
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 15:24:35 +0600

    complete parser logic

--- src/services/csv-mappers.ts
+++ src/services/csv-mappers.ts
@@ -3,9 +3,13 @@ import { csvFormat, csvParse } from 'd3-dsv';
 
 export class CsvToContactMapper implements CsvToEntityMapper<Contact> {
   map(csv: string): Contact[] {
-    const items = csvParse(csv);
-    debugger;
-    return [];
+    const items = [];
+
+    for (const item of csvParse(csv)) {
+      items.push(item as Contact);
+    }
+
+    return items;
   }
 }
 
