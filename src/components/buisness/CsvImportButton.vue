9e7bdddeffd227520213a48551a093be51cbc69f
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 13:01:22 +0600

    add csv import button component

--- src/components/buisness/CsvImportButton.vue
+++ src/components/buisness/CsvImportButton.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts">
+import { MESSAGE } from '@/constants';
+import type { AppFileInput as IAppFileInput } from '@/interfaces';
+import { ref } from 'vue';
+
+import AppFileInput from '../ui/AppFileInput.vue';
+
+const appFileInput = ref<IAppFileInput>();
+
+const $emit = defineEmits<{
+  change: [value: string];
+}>();
+
+function onChange() {
+  const reader = new FileReader();
+  reader.onload = () => {
+    $emit('change', reader.result as string);
+  };
+  reader.readAsText(appFileInput.value!.fileInput.files![0]);
+}
 </script>
 
-<template></template>
+<template>
+  <AppFileInput
+    id="csv-import"
+    ref="appFileInput"
+    accept=".csv"
+    @change="onChange()">
+    {{ MESSAGE.IMPORT_CSV }}
+  </AppFileInput>
+</template>
 
 <style scoped></style>
