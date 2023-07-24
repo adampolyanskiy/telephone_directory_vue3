980ea6eac5372fa9c4aefa9681dbeaaf2dc7e418
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 09:31:26 +0600

    implement all functionality except csv & update

--- src/components/buisness/TelephoneDirectory.vue
+++ src/components/buisness/TelephoneDirectory.vue
@@ -2,35 +2,91 @@
 import TelephoneDirectoryTable from '@/components/buisness/TelephoneDirectoryTable.vue';
 import AppButton from '@/components/ui/AppButton.vue';
 import FilterInput from '@/components/ui/FilterInput.vue';
-import { ContactService } from '@/constants';
+import { ContactService, MODAL_NAMES } from '@/constants';
+import { ModalService } from '@/constants';
+import { MESSAGE } from '@/constants';
 import type { Contact, ContactService as IContactService } from '@/interfaces';
-import { inject, reactive, ref, watch } from 'vue';
+import type { ModalService as IModalService } from '@/interfaces';
+import { inject, ref, shallowRef, unref, watchEffect } from 'vue';
 
+import ContactForm from './ContactForm.vue';
+
+const $modal = inject<IModalService>(ModalService);
 const contactService = inject<IContactService>(ContactService);
+const emptyContact = {
+  firstname: '',
+  lastname: '',
+  middlename: '',
+  address: '',
+  homephone: '',
+  cellphone: ''
+};
+const addFormValid = ref<boolean>(false);
+const filter = ref<string>('');
+const contacts = ref<Contact[]>([]);
+const formValue = shallowRef<Contact>({ ...emptyContact });
+const filteredContacts = shallowRef<Contact[]>([]);
 
-const filter = ref<string>();
+async function addContact(contact?: Contact) {
+  if (!contact) {
+    const res = await $modal!.open(MODAL_NAMES.CONTACT_ADD);
 
-const contacts = reactive<Contact[]>([]);
+    if (!res) {
+      clearFormValue();
+      return;
+    }
+  }
 
-let filteredContacts = contacts;
+  const item = contact ? contact : unref(formValue.value);
 
-function generateRandomContact() {
-  contacts.push(contactService!.generateRandomContact());
+  if (!contact) {
+    clearFormValue();
+  }
+
+  if ((await contactService?.add(item)) === -1) {
+    await $modal!.open(MODAL_NAMES.CONTACT_DUPLICATE);
+    return;
+  }
+
+  await getContacts();
 }
 
-watch(filter, () => {
-  filteredContacts = contactService!.filterContacts(contacts, filter.value!);
+async function getContacts() {
+  contacts.value = await contactService!.getAll();
+}
+
+function clearFormValue() {
+  formValue.value = { ...emptyContact };
+}
+
+async function deleteContact(id: number) {
+  const res = await $modal!.open(MODAL_NAMES.CONTACT_DELETE);
+
+  if (res) {
+    await contactService?.delete(id);
+    await getContacts();
+  }
+}
+
+await getContacts();
+
+watchEffect(() => {
+  filteredContacts.value = contactService!.filterContacts(contacts.value, filter.value!);
 });
 </script>
 
 <template>
-  <main class="p-10">
+  <main class="py-10 px-36">
     <div class="bg-gray-50 mb-4 p-3 rounded-lg drop-shadow-md flex">
-      <AppButton class="mr-3">Добавить</AppButton>
       <AppButton
         class="mr-3"
-        @click="generateRandomContact()"
-        >Сгенерировать</AppButton
+        @click="addContact()"
+        >{{ MESSAGE.ADD }}</AppButton
+      >
+      <AppButton
+        class="mr-3"
+        @click="addContact(contactService!.generateRandomContact())"
+        >{{ MESSAGE.GENERATE }}</AppButton
       >
       <FilterInput
         v-model.trim="filter"
@@ -40,10 +96,43 @@ watch(filter, () => {
     <div>
       <TelephoneDirectoryTable
         class="rounded-lg drop-shadow-lg"
-        :contacts="filteredContacts">
+        :contacts="filteredContacts"
+        @contact-delete="deleteContact($event)">
       </TelephoneDirectoryTable>
     </div>
   </main>
+
+  <AppPopup
+    :name="MODAL_NAMES.CONTACT_DELETE"
+    :message="MESSAGE.DELETE_ITEM"
+    :accept="MESSAGE.YES"
+    :cancel="MESSAGE.NO">
+  </AppPopup>
+
+  <AppPopup
+    :name="MODAL_NAMES.CONTACT_DUPLICATE"
+    :message="MESSAGE.ITEM_DUPLICATE"
+    :accept="MESSAGE.OK">
+  </AppPopup>
+
+  <AppModal
+    :name="MODAL_NAMES.CONTACT_ADD"
+    :title="MESSAGE.TITLES.ADD"
+    :accept="MESSAGE.SAVE"
+    :cancel="MESSAGE.CANCEL"
+    :accept-button-enabled="!addFormValid">
+    <ContactForm
+      v-model="formValue"
+      @valid="addFormValid = $event"></ContactForm>
+  </AppModal>
+
+  <AppModal
+    :name="MODAL_NAMES.CONTACT_UPDATE"
+    :title="MESSAGE.TITLES.UPDATE"
+    :accept="MESSAGE.SAVE"
+    :cancel="MESSAGE.CANCEL">
+    <ContactForm></ContactForm>
+  </AppModal>
 </template>
 
 <style scoped></style>
