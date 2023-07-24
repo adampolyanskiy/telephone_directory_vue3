f85f3cb7cd0826b69ca26c57384b472e6e4c6031
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 15:24:52 +0600

    implement buttons template

--- src/components/buisness/CsvExportButton.vue
+++ src/components/buisness/CsvExportButton.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { MESSAGE } from '@/constants';
 </script>
 
-<template></template>
+<template>
+  <button
+    type="button"
+    class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+    {{ MESSAGE.EXPORT_CSV }}
+  </button>
+</template>
 
 <style scoped></style>
