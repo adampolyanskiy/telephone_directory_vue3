26e862b5cee5a37e7c6e130142981baece289b83
adam.p@eyeride.io <adam.p@eyeride.io>
2023-07-24 08:27:00 +0600

    wrapped main component with suspense and provided fallback

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@ import TelephoneDirectory from './components/buisness/TelephoneDirectory.vue';
 </script>
 
 <template>
-  <TelephoneDirectory></TelephoneDirectory>
+  <Suspense>
+    <TelephoneDirectory></TelephoneDirectory>
+    <template #fallback>
+      <main class="p-10">
+        <div class="bg-gray-50 mb-4 p-8 rounded-lg drop-shadow-md animate-pulse"></div>
+        <div class="bg-gray-50 mb-4 p-4 rounded-lg drop-shadow-md animate-pulse"></div>
+      </main>
+    </template>
+  </Suspense>
 </template>
 
 <style scoped></style>
